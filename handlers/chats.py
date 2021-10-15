ace34cbb494014d261019bda67ad8bc575bd01d4
isaevvictorm <vitya-isaev@mail.ru>
2021-10-15 03:03:03 +0300

    Правка

--- handlers/chats.py
+++ handlers/chats.py
@@ -21,7 +21,7 @@ def send_message(jsn):
             SELECT
                 '{0}' as chat_id,
                 '{1}' as text,
-                {2} as from_me
+                {2} as from_me,
                 '{3}' as answer_for;
             '''.format(jsn['chat__id'] if 'chat__id' in jsn else '', jsn['text'] if 'text' in jsn else '', jsn['from_me'] if 'from_me' in jsn else '', jsn['answer_for'] if 'answer_for' in jsn else ''))
         bot = telebot.TeleBot(setting['TOKEN'])
