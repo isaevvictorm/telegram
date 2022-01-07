ee2a5624b3fad01b95ca2427777741da46361325
Виктор Исаев <vitya-isaev@mail.ru>
2022-01-07 23:04:32 +0300

    Правка

--- handlers/modules/nlp.py
+++ handlers/modules/nlp.py
@@ -7,8 +7,8 @@ import nltk
 
 def filter_text(text):
     text = text.lower()
-    #text = [c for c in text if c in setting['SYMBOL']]
-    #text = ''.join(text)
+    text = [c for c in text if c in setting['SYMBOL']]
+    text = ''.join(text)
     return text
 
 
