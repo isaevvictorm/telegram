2b59157b60cd5c82b2305fe90a1070e81b820530
Виктор Исаев <vitya-isaev@mail.ru>
2022-01-07 07:11:52 +0300

    Правка

--- handlers/migration.py
+++ handlers/migration.py
@@ -145,27 +145,6 @@ def migration():
         );
     ''')
 
-    dt = db.exec('''
-        Select count(*) as cnt from Template;
-    ''')
-    print('Count:', dt.table[0]['cnt'])
-    if str(dt.table[0]['cnt']) == '0':
-    
-        with io.open(os.path.join(os.getcwd() + "/startup/dialogues.txt"), newline='', encoding="utf-8", errors='replace') as f:
-            content = f.read()
-            
-            for replicas in [dialogue_line.split('\n') for dialogue_line in content.split('\n\n')]:
-                if len(replicas) < 2:
-                    continue
-                
-                question, answer = replicas[:2]
-                question = question[2:]
-                answer = answer[2:]
-
-                print(question, answer)
-
-   
-
     # ----------------------
     # Таблица "IMG"
     # ----------------------
--- test.py
+++ test.py
@@ -1,16 +1,23 @@
 import os
-import codecs
 import io
 
-with io.open(os.path.join(os.getcwd() + "/startup/dialogues.txt"), newline='', encoding="utf-8", errors='') as f:
-    content = f.read()
-    
-    for replicas in [dialogue_line.split('\n') for dialogue_line in content.split('\n\n')]:
-        if len(replicas) < 2:
-            continue
-        
-        question, answer = replicas[:2]
-        question = question[2:]
-        answer = answer[2:]
+dt = db.exec('''
+    Select count(*) as cnt from Template;
+''')
+print('Count:', dt.table[0]['cnt'])
+if str(dt.table[0]['cnt']) == '0':
 
-        print(question, answer)
+    with io.open(os.path.join(os.getcwd() + "/startup/dialogues.txt"), newline='', encoding="utf-8", errors='replace') as f:
+        content = f.read()
+        
+        for replicas in [dialogue_line.split('\n') for dialogue_line in content.split('\n\n')]:
+            if len(replicas) < 2:
+                continue
+            
+            question, answer = replicas[:2]
+            question = question[2:]
+            answer = answer[2:]
+
+            print(question, answer)
+
+   
